7114b5a11f01614141356806d80ff3cc7ac2ccdb
hill1028 <1607556129@qq.com>
2020-03-30 11:01:11 +0800

    first commit

---
+++ src/components/cart-control/cart-control.vue
@@ -0,0 +1,82 @@
+<template>
+  <div class="cartcontrol">
+    <transition name="move">
+      <div class="cart-decrease" v-show="food.count>0" @click.stop="decrease">
+        <span class="inner icon-remove_circle_outline"></span>
+      </div>
+    </transition>
+    <div class="cart-count" v-show="food.count>0">{{food.count}}</div>
+    <div class="cart-add icon-add_circle" @click.stop="add($event)"></div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'cart-control',
+    props: {
+      food: {
+        type: Object,
+        default() {
+          return {}
+        }
+      }
+    },
+    methods: {
+      add(event) {
+        if (!event._constructed) {
+          return
+        }
+        if (this.food.count === undefined) {
+          this.$set(this.food, 'count', 1)
+          console.log(this.food.count)
+        } else {
+          console.log(this.food.count)
+          this.food.count++
+        }
+      },
+      decrease() {
+        if (this.food.count) {
+          this.food.count--
+        }
+      }
+    }
+  }
+</script>
+
+<style lang="stylus" scoped>
+  @import "~common/stylus/variable"
+
+  .cartcontrol
+    display: flex
+    align-items: center
+    .cart-decrease
+      display: inline-block
+      padding: 6px
+      opacity: 1
+      .inner
+        display: inline-block
+        line-height: 24px
+        font-size: $fontsize-large-xxx
+        color: $color-blue
+        transition: all 0.4s linear
+        transform: rotate(0)
+      &.move-enter-active, &.move-leave-active
+        transition: all 0.4s linear
+      &.move-enter, &.move-leave-active
+        opacity: 0
+        transform: translate3d(24px, 0, 0)
+        .inner
+          transform: rotate(180deg)
+    .cart-count
+      width: 12px
+      line-height: 24px
+      text-align: center
+      font-size: $fontsize-small-s
+      color: $color-grey
+    .cart-add
+      display: inline-block
+      padding: 6px
+      line-height: 24px
+      font-size: $fontsize-large-xxx
+      color: $color-blue
+</style>
---
+++ src/components/shop-cart/shop-cart.vue
@@ -0,0 +1,143 @@
+<template>
+<div class="shop-cart">
+  <div class="content">
+    <div class="content-left">
+      <div class="logo-wrapper">
+        <div class="logo" :class="{'highlight': totalCount>0}">
+          <i class="icon-shopping_cart" :class="{'highlight': totalCount>0}"></i>
+        </div>
+        <div class="num" v-show="totalCount>0">{{totalCount}}</div>
+      </div>
+      <div class="price" :class="{'highlight': totalPrice>0}">￥{{totalPrice}}</div>
+      <div class="desc">另需配送费{{deliveryPrice}}</div>
+    </div>
+    <div class="content-right">
+      <div class="pay" :class="payClass"></div>
+    </div>
+  </div>
+</div>
+</template>
+
+<script>
+  export default {
+    name: 'shop-cart',
+    props: {
+      deliveryPrice: {
+        type: Number,
+        default() {
+          return 0
+        }
+      },
+      minPrice: {
+        type: Number,
+        default: 0
+      },
+      selectFoods: {
+        type: Array,
+        default () {
+          return []
+        }
+      }
+    },
+    computed: {
+      totalPrice() {
+        let price = 0
+        this.selectFoods.forEach((food) => {
+          if (food.count) {
+            price += food.count * food.price
+          }
+        })
+        return price
+      },
+      totalCount() {
+        let count = 0
+        this.selectFoods.forEach((food) => {
+          if (food.count) {
+            count += food.count
+          }
+        })
+        return count
+      },
+      payClass() {
+        if (this.selectFoods.count || this.totalPrice < this.minPrice) {
+          return 'not-enough'
+        } else {
+          return 'enough'
+        }
+      }
+    }
+  }
+</script>
+
+<style lang="stylus" scoped>
+  @import "~common/stylus/mixin"
+  @import "~common/stylus/variable"
+  .shop-cart
+    height: 100%
+    .content
+      display: flex
+      backgroud-color: $color-background
+      color: $color-light-grey
+      .content-left
+        flex: 1
+        .logo-wrapper
+          /*position: relative*/
+          display: inline-block
+          vertical-align: top
+          top: -10px
+          margin: 0 12px
+          padding: 6px
+          height: 56px
+          width: 56px
+          box-sizing: border-box
+          border-radius: 50%
+          backgroud-color: $color-background
+          .logo
+            width: 100%
+            height: 100%
+            border-radius: 50%
+            text-align: center
+            background-color: $color-dark-grey
+            &.highlight
+              background-color: $color-blue
+            .icon-shopping_cart
+              line-height: 44px
+              font-size: $fontsize-large-xxx
+              color: $color-light-grey
+              &.highlight
+                color: $color-white
+          .num
+            position: absolute
+            top: 0
+            right: 0
+        .price
+          display: inline-block
+          vertical-align: top
+          margin-top: 12px
+          padding-right: 12px
+          box-sizing: border-box
+          border-right: 1px solid rgba(255, 255, 255, 0.1)
+          font-weight: 700
+          font-size: $fontsize-large
+          &.highlight
+            color: $color-white
+        .desc
+          display: inline-block
+          vertical-align: top
+          margin: 12px 0 0 12px
+          line-height: 24px
+          font-size: $fontsize-small-s
+      .content-right
+        flex: 0 0 105px
+        .pay
+          height: 48px
+          line-height: 48px
+          text-align: center
+          font-weight: 700
+          font-size: $fontsize-small
+          &.not-enough
+            background-color: $color-dark-grey
+          &.enough
+            background: $color-green
+            color: $color-white
+</style>
