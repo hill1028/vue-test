98dae89c6fcb4ddebce1af4828031cdbf1a60f23
hill1028 <1607556129@qq.com>
2020-04-20 16:53:59 +0800

    4-20

--- src/components/ratings/ratings.vue
+++ src/components/ratings/ratings.vue
@@ -1,5 +1,12 @@
 <template>
-    <div>I am ratings</div>
+    <cube-scroll class="ratings">
+      <div class="ratings-content">
+        <div class="overview">
+          <div class="overview-left"></div>
+          <div class="overview-right"></div>
+        </div>
+      </div>
+    </cube-scroll>
 </template>
 
 <script>
@@ -8,6 +15,16 @@
   }
 </script>
 
-<style scoped>
-
+<style lang="stylus" scoped>
+  .ratings
+    height: 100%
+    .ratings-content
+      display: flex
+      padding: 18px 0
+      .overview-left
+        flex: 0 0 137px
+        width: 137px
+        padding: 6px 0
+        border-right: 1px solid $color-col-line
+        text-align: center
 </style>
